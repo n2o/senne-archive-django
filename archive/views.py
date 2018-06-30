c54692f72e34e23ad862c2cf6e39b7b0d4e53f0b
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-07-01 00:49:58 +0200

    Add paginator to results

--- archive/views.py
+++ archive/views.py
@@ -1,5 +1,6 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
-from django.http import HttpRequest, Http404
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 from typing import List
 
@@ -8,18 +9,24 @@ from .models import Item
 
 def index(request: HttpRequest):
     query = request.GET.get("q")
+    page = request.GET.get("page", 1)
     items = []
     if query:
-        items: List[Item] = Item.objects \
+        items_db: List[Item] = Item.objects \
             .filter(public=True) \
-            .filter(Q(title__icontains=query)) \
-            .filter(Q(abstract__icontains=query))
-    return render(request, "archive/index.html", {"items": items})
+            .filter(Q(title__icontains=query) |
+                    Q(abstract__icontains=query))
+        paginator = Paginator(items_db, 20)
+        try:
+            items = paginator.page(page)
+        except PageNotAnInteger:
+            items = paginator.page(1)
+        except EmptyPage:
+            items = paginator.page(paginator.num_pages)
+    return render(request, "archive/index.html", {"items": items, "forloop_modifier": 20 * (int(page) - 1)})
 
 
 def detail(request: HttpRequest, pk: Item):
     item_db = get_object_or_404(Item, pk=pk)
     item = item_db if item_db.public else None
-    if not item:
-        raise Http404("Archiveintrag konnte nicht gefunden werden")
     return render(request, "archive/detail.html", {"item": item})
