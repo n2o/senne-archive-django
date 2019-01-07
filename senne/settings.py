a1d0d5a6635df5d69f3df0889a98388495d32ade
Christian Meter <christian.meter@uni-duesseldorf.de>
2019-01-07 22:22:58 +0100

    Update deps

--- senne/settings.py
+++ senne/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+FILE_UPLOAD_PERMISSIONS = 0o777
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
